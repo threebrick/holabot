776ac79271b49ca2a063c137cf38d0ba022730e5
threebrick <darnell@threebrick.com>
2016-08-15 01:00:33 -0400

    updated app.js

--- app.js
+++ app.js
@@ -13,11 +13,11 @@ server.listen(process.env.port || process.env.PORT || 3978, function () {
   
 // Create chat bot
 var connector = new builder.ChatConnector({
-//    appId: process.env.MICROSOFT_APP_ID,
-//    appPassword: process.env.MICROSOFT_APP_PASSWORD
+    appId: process.env.MICROSOFT_APP_ID,
+    appPassword: process.env.MICROSOFT_APP_PASSWORD
 
-	appId: '34dd6a20-28bc-4813-8bff-73118d350176',
-    appPassword: 'ajLVekOosCvBHi8yrbr8By6'
+//	appId: '34dd6a20-28bc-4813-8bff-73118d350176',
+//    appPassword: 'ajLVekOosCvBHi8yrbr8By6'
 });
 var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
